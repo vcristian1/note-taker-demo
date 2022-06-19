063bd434192885ee7c0bf8527097e39c9605ddf6
Cristian Vargas <cristian.v0223@gmail.com>
2022-06-19 11:13:42 -0500

    added middleware to server.js, and added both HTML get routes

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 // import the express package
 const express = require("express")
+const path = require("path")
 const fs = require("fs")
 
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express()
 
-// 2 GET HTML Routes, and 2 API Routes: GET API, POST API below.
+// middleware to parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
+// 2 GET HTML Routes,
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
+// 2 API Routes: GET API, POST API below.
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        const newNote = JSON.parse(data)
+        res.json(newNote)
+        if (err) throw err;
+    });
+});
+
+// app.post('/api/notes', (req, res) =>
+    
+// );
+
+// GET Wildcard route 
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 
 // app.listen(PORT below
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
