47cce19bc512b19112ce233c4fcae8a654e0440e
Cristian Vargas <cristian.v0223@gmail.com>
2022-06-19 09:38:51 -0500

    imported fs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import the express package
 const express = require("express")
+const fs = require("fs")
 
 
 const PORT = 3001;
@@ -7,8 +8,7 @@ const PORT = 3001;
 // Initialize our app variable by setting it to the value of express()
 const app = express()
 
-// routes below
-
+// 2 GET HTML Routes, and 2 API Routes: GET API, POST API below.
 
 
 
