28bc34390e5d4b2431bc444b5e7f454fd1d9d6c6
Cristian Vargas <cristian.v0223@gmail.com>
2022-06-19 09:18:58 -0500

    created server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+// import the express package
+const express = require("express")
+
+
+const PORT = 3001;
+
+// Initialize our app variable by setting it to the value of express()
+const app = express()
+
+// routes below
+
+
+
+
+
+
+
+// app.listen(PORT below
