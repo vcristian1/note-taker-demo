4def892ebf4e63535b1b5addee053f92d85fa2b3
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-22 22:06:06 -0500

    second commit

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-// import the express package
-const express = require("express")
-const path = require("path")
-const fs = require("fs")
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
-// Initialize our app variable by setting it to the value of express()
 const app = express();
 
-// middleware to parse data
+//Middleware to parse data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // 2 GET HTML Routes,
-// GET Wildcard route 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-
+// GET /notes route to return notes.html
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+// GET Wildcard route to direct users to index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 // 2 API Routes: GET API, POST API below.
 
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
-        const newNote = JSON.parse(data)
-        res.json(newNote)
-        if (err) throw err;
-    });
-});
+        const notesFromFile = JSON.parse(data)
+        res.json(notesFromFile)
+    })
+})
 
 app.post('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
@@ -46,6 +44,21 @@ app.post('/api/notes', (req, res) => {
             if (err) throw err;
         });
         res.json(newNote);
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const notesArray = notes.filter( item => {
+            return item.id !==  noteID
+        });
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
+            if (err) throw err;
+            res.json(notesArray)
+        });
     });
 });
 
